6c66b1c9d8937d01be1df4eff93b8e0fa47b76e7
Vadym <vadim.rolex.2005@gmail.com>
2025-08-06 15:34:37 +0300

    Built module and created out folder.

--- src/main/java/com/project/configuration/BotConfig.java
+++ src/main/java/com/project/configuration/BotConfig.java
@@ -8,20 +8,9 @@ public class BotConfig {
     private final String _botToken;
 
     public BotConfig() {
-        Dotenv dotenv = null;
-
-        try {
-            dotenv = Dotenv.configure().ignoreIfMissing().load();
-        } catch (Exception ignored) {
-        }
-
-        _botUsername = System.getenv("BOT_USERNAME") != null ?
-                System.getenv("BOT_USERNAME") :
-                (dotenv != null ? dotenv.get("BOT_USERNAME") : null);
-
-        _botToken = System.getenv("BOT_TOKEN") != null ?
-                System.getenv("BOT_TOKEN") :
-                (dotenv != null ? dotenv.get("BOT_TOKEN") : null);
+        Dotenv dotenv = Dotenv.configure().load();
+        _botUsername = dotenv.get("BOT_USERNAME");
+        _botToken = dotenv.get("BOT_TOKEN");
     }
 
     public String getBotUsername() {
