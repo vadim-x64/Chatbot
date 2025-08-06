6a2366f451a246fce96a4b5673efe3358d9ffa72
Vadym <vadim.rolex.2005@gmail.com>
2025-08-06 15:50:42 +0300

    Fixed jar archieve.

--- src/main/java/com/project/configuration/BotConfig.java
+++ src/main/java/com/project/configuration/BotConfig.java
@@ -1,16 +1,13 @@
 package com.project.configuration;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 public class BotConfig {
 
     private final String _botUsername;
     private final String _botToken;
 
     public BotConfig() {
-        Dotenv dotenv = Dotenv.configure().load();
-        _botUsername = dotenv.get("BOT_USERNAME");
-        _botToken = dotenv.get("BOT_TOKEN");
+        _botUsername = System.getenv("BOT_USERNAME");
+        _botToken = System.getenv("BOT_TOKEN");
     }
 
     public String getBotUsername() {
