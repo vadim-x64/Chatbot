a3368deba6a484c2ba288d615c1ef0038445c710
Vadym <vadim.rolex.2005@gmail.com>
2025-11-29 14:17:04 +0200

    Edited main file.

--- src/main/java/com/project/Main.java
+++ src/main/java/com/project/Main.java
@@ -2,9 +2,22 @@ package com.project;
 
 import com.project.initializer.BotInitializer;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.net.InetSocketAddress;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", exchange -> {
+            String response = "Bot is running";
+            exchange.sendResponseHeaders(200, response.length());
+            exchange.getResponseBody().write(response.getBytes());
+            exchange.close();
+        });
+        server.setExecutor(null);
+        server.start();
+        //System.out.println("HTTP server started on port 8080");
         try {
             BotInitializer botInitializer = new BotInitializer();
             botInitializer.initializeBot();
